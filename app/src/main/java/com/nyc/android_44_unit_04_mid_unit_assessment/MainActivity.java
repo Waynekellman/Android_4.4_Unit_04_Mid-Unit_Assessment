3b00c68a9a02327dba30e60cd5d150d8c86f999f
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-12-20 19:13:42 -0500

    initial commit

---
+++ app/src/main/java/com/nyc/android_44_unit_04_mid_unit_assessment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nyc.android_44_unit_04_mid_unit_assessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
