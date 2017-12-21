446ff017af4b73f2d651d58c7e9b5e64688e6ef5
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-12-20 19:29:20 -0500

    complete part01

--- app/src/main/java/com/nyc/android_44_unit_04_mid_unit_assessment/MainActivity.java
+++ app/src/main/java/com/nyc/android_44_unit_04_mid_unit_assessment/MainActivity.java
@@ -1,7 +1,12 @@
 package com.nyc.android_44_unit_04_mid_unit_assessment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.nyc.android_44_unit_04_mid_unit_assessment.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MainFragment mainFragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, mainFragment);
+        fragmentTransaction.commit();
+        Log.d("MainFragement","MainFragmentRan");
     }
 }
